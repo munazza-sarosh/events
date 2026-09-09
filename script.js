c77a9c154ff699c358b965675f3bf9e5682a4536
munazza-sarosh <saroshmunazza@gmail.com>
2026-09-09 22:08:30 +0500

    using events in js

---
+++ script.js
@@ -0,0 +1,66 @@
+
+function myFunction() {
+    alert("Hello! I am an alert box!");
+}
+
+function demo(hello) {
+    console.log(hello);
+    console.log(hello.keyCode);
+    console.log(hello.code);
+    let minekey = hello.key;
+    if (minekey == "Enter") {
+        alert("You have pressed Enter key");
+    }
+
+}
+
+function escape(Hello) {
+    console.log(Hello);
+    console.log(Hello.keyCode);
+    console.log(Hello.code);
+    let key = Hello.key;
+    if (key == "Escape") {
+        alert("you pressed an escape key");
+    }
+}
+// on click
+document.getElementById("element").onclick = function () { element() };
+function element() {
+    document.getElementById("element").innerHTML = "YOU CLICKED ME!";
+}
+
+// context menu
+document.getElementById("contextmenu").oncontextmenu = function () { context() };
+function context() {
+    document.getElementById("contextmenu").innerHTML = "YOU CLICKED ME!";
+}
+
+// double click
+document.getElementById("doubleclick").ondblclick = function () { dblclick() };
+function dblclick() {
+    document.getElementById("doubleclick").innerHTML = "YOU CLICKED ME!";
+}
+
+// mouse down
+document.getElementById("mousedown").onmousedown = function () { mousedown() };
+function mousedown() {
+    document.getElementById("mousedown").innerHTML = "YOU CLICKED ME!";
+}
+
+// mouse enter
+document.getElementById("mouse-enter").onmouseenter = function () { mouse_enter() };
+function mouse_enter() {
+    document.getElementById("mouse-enter").innerHTML = "YOU CLICKED ME!";
+}
+
+// mouse leave
+document.getElementById("mouse-leave").onmouseleave = function () { mouseleave() };
+function mouseleave() {
+    document.getElementById("mouse-leave").innerHTML = "YOU CLICKED ME!";
+}
+
+// mouse move
+document.getElementById("mouse-move").onmousemove = function () { mousemove() };
+function mousemove() {
+    document.getElementById("mouse-move").innerHTML = "YOU CLICKED ME!";
+}
